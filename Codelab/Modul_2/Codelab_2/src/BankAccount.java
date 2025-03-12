c730b8d6e22916f469349797b9cd5e3e03b589b2
alfhyy <alfhyands@gmail.com>
2025-03-12 09:36:08 +0700

    notes in codelab2

--- Codelab/Modul_2/Codelab_2/src/BankAccount.java
+++ Codelab/Modul_2/Codelab_2/src/BankAccount.java
@@ -1,14 +1,18 @@
 public class BankAccount {
+    //declare necessary variables
     String accountNumber;
     String ownerName;
     double balance;
 
+    //initialized constructor
     public BankAccount(String accountNumber, String ownerName, double balance) {
+        //scope for defining where a certain variable or method is accessible in a program.
         this.accountNumber = accountNumber;
         this.ownerName = ownerName;
         this.balance = balance;
     }
 
+    //make displayInfo() function to display user' information
     public void displayInfo() {
         System.out.println("Account Number :" + accountNumber);
         System.out.println("Account Name :" + ownerName);
@@ -16,17 +20,22 @@ public class BankAccount {
         System.out.println();
     }
 
+    //make depositMoney() function to allows user make a deposit
     public void depositMoney(double amount) {
+        //put more into amount depending on how much the balance
         balance += amount;
         System.out.println(ownerName + " just deposit :" + amount + " your balance :" + balance);
     }
 
+    //make withdrawMoney() function to allows user make a withdraw
     public void withdrawMoney(double amount) {
+        //condition if user' balance is less than the amount of withdraw
         if (amount > balance) {
             System.out.println(ownerName + " just withdraw :" + amount + " Insufficient balance, please try again!");
             return;
         }
 
+        //reduce the balance from the withdraw amount
         balance -= amount;
         System.out.println(ownerName + " just withdraw :" + amount + " your balance :" + balance);
         System.out.println();
--- Codelab/Modul_2/Codelab_2/src/Main.java
+++ Codelab/Modul_2/Codelab_2/src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        //importing another class named BankAccount and split into two variables named
         BankAccount bankAccount1 = new BankAccount("202410370110423", "Viero", 5000000);
         BankAccount bankAccount2 = new BankAccount("202410370110506", "Opan", 4000000);
 
+        //calling the methods to display, deposit and withdraw for 1st person
         bankAccount1.displayInfo();
         bankAccount1.depositMoney(400000);
         bankAccount1.withdrawMoney(1000);
 
+        //calling the methods to display, deposit and withdraw for 2nd person
         bankAccount2.displayInfo();
         bankAccount2.depositMoney(300000);
         bankAccount2.withdrawMoney(300000000);
