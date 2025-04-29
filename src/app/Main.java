191f1b9f36b1b64e93e15b052bec5ba4df8cab27
alfhyy <alfhyands@gmail.com>
2025-04-29 09:15:06 +0700

    Codelab polymorph

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,22 +7,22 @@ public class Main {
         Anggota viero = new Anggota("Viero Alfiandhy", "A-423");
         Anggota opan = new Anggota("Ovan Keyva", "A-506");
 
-        Buku madilog = new Fiksi("Takehiko Inoue", "Vagabond");
+        Buku vagabond = new Fiksi("Takehiko Inoue", "Vagabond");
         Buku homoSapiens = new NonFiksi("Yuval Noah Harari", "Sapiens: A Brief History of Humankind");
 
         viero.displayInfo();
         opan.displayInfo();
         System.out.println();
 
-        madilog.displayInfo();
+        vagabond.displayInfo();
         homoSapiens.displayInfo();
         System.out.println();
 
-        viero.borrowBook(madilog.getTitle());
+        viero.borrowBook(vagabond.getTitle());
         opan.borrowBook(homoSapiens.getTitle(), 7);
         System.out.println();
 
-        viero.returnBook(madilog.getTitle());
+        viero.returnBook(vagabond.getTitle());
         opan.returnBook(homoSapiens.getTitle());
     }
 }
